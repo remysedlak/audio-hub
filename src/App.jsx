6ce8d107e2541a9ecc7c746a1bd2cb48f24f657e
remysedlak <remysedlak@gmail.com>
2025-02-23 00:26:18 -0500

    add Canvas component, update dependencies, and enhance App layout with UploadButton and improved text

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,39 @@ import React from 'react';
 import './tailwind.css'; // Ensure Tailwind CSS is imported
 import AudioVisualizer from './comps/AudioVisualiser.jsx';
 import KeyInformation from './comps/KeyInformation.jsx';
+import UploadButton from './comps/UploadButton.jsx';
 
 function App() {
 
   return (
     <div>
-        <h1 className="text-4xl text-center py-4">Audio Visualizer</h1>
-        <p className="text-center">Upload an audio file to see the visualization</p>
+        <h1 className="text-4xl text-center py-4 font-serif">Audio Visualizer</h1>
+        <p className="text-center font-serif">
+          This website transforms audio files into visual representations using the Web Audio API.
+          </p>
 
-        <div className="flex justify-center py-10">
+        <div className="border hover:bg-gray-100 flex justify-center py-10">
+          <UploadButton />
+        </div>
+
+        <h1 className="text-4xl text-center py-4 font-serif">Key Information</h1>
+        <p className="text-center font-serif">
+          Find's the key of the song you uploaded.
+        </p>
+
+
+
+        <div className="flex justify-center text-center py-10 font-serif">
           <AudioVisualizer />
         </div>
 
         <div className="flex justify-center py-10">
           <KeyInformation />
         </div>
+
+        
+
+        
         
     </div>
   );
