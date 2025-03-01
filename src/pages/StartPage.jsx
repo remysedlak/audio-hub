4e38f098100f75113d5f3a47dc007b10dac5eeaa
remysedlak <remysedlak@gmail.com>
2025-03-01 11:58:33 -0500

    replace App component content with StartPage and add StartPage component for improved user experience

---
+++ src/pages/StartPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const StartPage = () => {
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            console.log('Selected file:', file);
+            // Handle the file upload logic here
+        }
+    };
+
+    return (
+        <div className="flex flex-col items-center mt-10">
+            <h1 className="mb-10 text-4xl text-center font-bold text-gray-800 mb-4">Welcome to harmoniX</h1>
+            <div>
+                <label className="cursor-pointer">
+                    <h4 className="text-2xl text-center font-bold text-gray-400 mb-4">Upload file to begin</h4>
+                    <input 
+                        type="file" 
+                        accept="audio/*" 
+                        className="hidden" 
+                        onChange={handleFileChange} 
+                    />
+                </label>
+            </div>
+        </div>
+    );
+};
+
+export default StartPage;
