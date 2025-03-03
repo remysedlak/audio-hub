cf2fa5f8f72068802357454a2e245082df759ab0
remysedlak <remysedlak@gmail.com>
2025-03-03 14:20:43 -0500

    Setting up more framework to add tools to

--- src/comps/SongOverview.jsx
+++ src/comps/SongOverview.jsx
@@ -5,7 +5,10 @@ const SongOverview = ({file}) => {
     return (
         <div className="p-4 bg-gray-800 text-white rounded-lg shadow-md">
             
-            <h4 className="text-xl font-semibold">Spectrogram</h4>
+            <h4 className="text-xl font-semibold py-2">Spectrogram</h4>
+            <canvas id="spectrogram" className="w-full h-40"></canvas>
+            <h4 className="text-xl font-semibold">Key</h4>
+            <h4 className="text-xl font-semibold">BPM </h4>
             
         </div>
     );
--- src/pages/AudioPage.jsx
+++ src/pages/AudioPage.jsx
@@ -16,23 +16,27 @@ const AudioPage = ({ file }) => {
   return( <>
     <h1 className="mb-10 text-4xl text-center font-bold mb-4 mt-4 pb-1 text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-white">Remy's Audio Hub</h1>
     {/* container for live and overview of song */}
-    <div className="flex small:flex-col w-full px-2 h-screen justify-around"> 
+    <div className="flex small:flex-col w-full px-4 h-screen justify-around"> 
       
       {/* {audioSrc && (
         <audio controls src={audioSrc} className="mb-10">
           Your browser does not support the audio element.
         </audio>
       )} */}
-      <div className="w-1/2 pr-1">
+      <div className="w-1/2 pr-2">
         <h4 className="text-2xl text-center font-bold text- mb-4">Live Song Info</h4>
         <MusicPlayer file={file}/>
       </div>
-      <div className="w-1/2 pl-1">
+      <div className="w-1/2 pl-2">
         <h4 className="text-2xl text-center font-bold text-white mb-4">Song Overview</h4>
         <SongOverview/>
       </div>
+      
   
     </div>
+    <p className="text-sm mt-2  opacity-10 text-center pb-2">
+				&copy; {new Date().getFullYear()} Remy Sedlak. Feel free to use and share this content.
+			</p>
     </>
     
   );
